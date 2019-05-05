Fix KLD computation

sp_vae_gan/loss_utils.py
@@ -17,10 +17,9 @@ def format_loss_dict(loss):
     )
 
 
-def kld_loss(z):
-    z_mean, z_var = z.mean(dim=1), z.var(dim=1)
-    z_logvar = torch.log(z_var.clamp_(min=1e-12))
-    return - 0.5 * (1 + z_logvar - z_mean.pow(2) - z_var)
+def kld_loss(mean, var):
+    logvar = torch.log(var.clamp_(min=1e-12))
+    return - 0.5 * (1 + logvar - mean.pow(2) - var)
 
 
 def bce(var, target):

sp_vae_gan/trainer.py
@@ -154,7 +154,8 @@ class Trainer:
 
         # Compute KLD
         with torch.no_grad():
-            latent_loss = loss_utils.kld_loss(z).mean()
+            latent_loss = loss_utils.kld_loss(
+                z.mean(dim=0), z.var(dim=0)).mean()
         '''
         if update:
             beta_latent_loss = self.beta * latent_loss